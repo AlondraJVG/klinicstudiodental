21fda80d8b52f9334dafa8bc08d0f0b573027635
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-05 17:49:58 -0600

    revision bd

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'clave_secreta'
 
 # Conexión a MySQL en PythonAnywhere
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Klinical:chocoLATE.21@Klinical.mysql.pythonanywhere-services.com/Klinical$default'
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -21,6 +20,7 @@ class Usuario(db.Model):
     contrasena = db.Column(db.String(255))
     fecha_creacion = db.Column(db.DateTime)
 
+# Ruta principal de login
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -31,13 +31,14 @@ def login():
         if usuario and check_password_hash(usuario.contrasena, contrasena):
             session['usuario'] = usuario.nombre
             flash('Has iniciado sesión correctamente', 'success')
-            return ('la informacion es corresta')
-            #return redirect(url_for('dashboard'))
+            return 'La información es correcta'
+            # return redirect(url_for('dashboard'))
         else:
             flash('Correo o contraseña incorrectos', 'danger')
 
     return render_template('login.html')
 
+# Ruta del dashboard (área privada)
 @app.route('/dashboard')
 def dashboard():
     if 'usuario' in session:
@@ -45,15 +46,14 @@ def dashboard():
     else:
         return redirect(url_for('login'))
 
+# Cerrar sesión
 @app.route('/logout')
 def logout():
     session.pop('usuario', None)
     flash('Has cerrado sesión.', 'info')
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+# Ruta de registro de nuevos usuarios
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
@@ -75,3 +75,6 @@ def register():
         return redirect(url_for('login'))  # Redirigir al login después del registro
 
     return render_template('register.html')  # Mostrar el formulario de registro
+
+if __name__ == '__main__':
+    app.run(debug=True)
