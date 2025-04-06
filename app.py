8b0e51c9b25b8bb8575719ee12f4d489e8b39f8e
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-05 18:50:26 -0600

    revision bd7

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ db = SQLAlchemy(app)
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50))
-    correo = db.Column(db.String(100), unique=True)
-    contrasena = db.Column(db.String(255))
-    fecha_creacion = db.Column(db.DateTime)
+    nombre = db.Column(db.String(100), nullable=False)
+    apellidos = db.Column(db.String(100), nullable=False)
+    correo = db.Column(db.String(255), unique=True, nullable=False)
+    contrasena = db.Column(db.String(255), nullable=False)
 
 # Ruta principal de login
 @app.route('/', methods=['GET', 'POST'])
