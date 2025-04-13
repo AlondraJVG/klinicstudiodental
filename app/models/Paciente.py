ac78f34fafdefbd0f4594580f2bbaceb42b536ac
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-13 02:34:27 -0600

    prueba del nuevo menu8

--- app/models/Paciente.py
+++ app/models/Paciente.py
@@ -1,11 +1,10 @@
 from app import db
-from sqlalchemy import Date
 
 class Paciente(db.Model):
     __tablename__ = 'Pacientes'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100) , nullable=False)
-    apellido = db.Column(db.String(100) , nullable=False)
-    fecha_nacimiento = db.Column(Date, nullable=False)
-    correo = db.Column(db.String(100), nullable=False) 
-    telefono = db.Column(db.String(20), nullable=False) 
+    nombre = db.Column(db.String(100), nullable=False)
+    apellido = db.Column(db.String(100), nullable=False)
+    fecha_nacimiento = db.Column(db.Date, nullable=False)  
+    correo = db.Column(db.String(100), nullable=False)
+    telefono = db.Column(db.String(20), nullable=False)
