2efd86b21778464782412fbad2627ea7768be525
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-13 02:14:34 -0600

    prueba del nuevo menu2

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,19 +2,18 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
-db = SQLAlchemy()  
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)  
+    db.init_app(app)
 
-    # Importar rutas
     from app.routes.auth import auth_bp
     from app.routes.paciente import paciente_bp
 
-    app.register_blueprint(auth_bp)  
+    app.register_blueprint(auth_bp)
     app.register_blueprint(paciente_bp)
 
     return app
