94a67507b31e9f19cec1e065c59328a97df8a7ce
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-13 02:24:23 -0600

    prueba del nuevo menu4

--- app/models/Paciente.py
+++ app/models/Paciente.py
@@ -3,7 +3,7 @@ from app import db
 class Paciente(db.Model):
     __tablename__ = 'Pacientes'
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.varchar(100) , nullable=False)
+    nombre = db.Column(db.String(100) , nullable=False)
     apellido = db.Column(db.varchar(100) , nullable=False)
     fecha_nacimiento = db.Column(db.date , nullable=False)
     correo = db.Column(db.varchar(100) , nullable=False)
