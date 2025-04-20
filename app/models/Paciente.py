71f8c705a847d94da7b353e6ef5aac98e7ff4945
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-20 04:19:35 -0600

    Busqueda9

--- app/models/Paciente.py
+++ app/models/Paciente.py
@@ -9,15 +9,7 @@ class Paciente(db.Model):
     edad = db.Column(db.Integer, nullable=False)
     sexo = db.Column(db.String(20), nullable=False)
     tipo_sangre = db.Column(db.String(10), nullable=False)
-    correo = db.Column(db.String(100), unique=True)
+    correo = db.Column(db.String(100), nullable=False)
     telefono = db.Column(db.String(20), nullable=False)
     contacto_emergencia  = db.Column(db.String(20), nullable=False)
     nombre_contacto = db.Column(db.String(100), nullable=False)
-
-    __table_args__ = (
-    db.UniqueConstraint('nombre', 'apellido', 'correo', name='uq_nombre_apellido_correo'),
-    )
-@validates('nombre', 'apellido', 'correo')
-def convertir_minusculas(self, key, value):
-    return value.strip().lower()
-
