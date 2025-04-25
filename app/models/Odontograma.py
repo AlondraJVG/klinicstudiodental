2b4314be887b77fad64399af7cae36f721e7f9e8
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-25 00:43:31 -0600

    odontograma 10

--- app/models/Odontograma.py
+++ app/models/Odontograma.py
@@ -2,5 +2,9 @@ from app import db
 class Odontograma(db.Model):
     __tablename__ = 'odontograma'
     id = db.Column(db.Integer, primary_key=True)
-    paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
-    fecha_creacion = db.Column(db.DateTime)
+    paciente_id = db.Column(db.Integer, nullable=False)
+    fecha_creacion = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, paciente_id, fecha_creacion=None):
+        self.paciente_id = paciente_id
+        self.fecha_creacion = fecha_creacion or datetime.utcnow()
