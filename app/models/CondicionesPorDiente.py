5fa9c4b16431d1a06266c03de89cfc6f7ac9b80f
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-25 01:03:39 -0600

    odontograma 12

--- app/models/CondicionesPorDiente.py
+++ app/models/CondicionesPorDiente.py
@@ -1,16 +1,16 @@
 from app import db
 
 class CondicionesPorDiente(db.Model):
-    __tablename__ = 'condiciones_por_diente'
+    __tablename__ = 'Condiciones_por_Diente'
 
     id = db.Column(db.Integer, primary_key=True)
     diente_id = db.Column(db.Integer, db.ForeignKey('dientes.id'), nullable=False)
-    condicion_id = db.Column(db.Integer, db.ForeignKey('condiciones_dentales.id'), nullable=False)
+    condicion_id = db.Column(db.Integer, db.ForeignKey('Condiciones_por_Diente.id'), nullable=False)
     descripcion = db.Column(db.Text, nullable=True)
 
     # Relación de los modelos
-    diente = db.relationship('Diente', backref='condiciones_por_diente')
-    condicion = db.relationship('CondicionesDentales', backref='condiciones_por_diente')
+    diente = db.relationship('Diente', backref='Condiciones_por_Diente')
+    condicion = db.relationship('CondicionesDentales', backref='Condiciones_por_Diente')
 
     def __repr__(self):
         return f'<CondicionPorDiente {self.diente.numero_diente} - {self.condicion.nombre}>'
--- app/models/Odontograma.py
+++ app/models/Odontograma.py
@@ -2,10 +2,10 @@ from app import db
 from datetime import datetime
 
 class Odontograma(db.Model):
-    __tablename__ = 'odontogramas'
+    __tablename__ = 'Odontogramas'  
 
     id = db.Column(db.Integer, primary_key=True)
-    paciente_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'), nullable=False)
-    fecha_creacion = db.Column(db.Date, default=datetime.utcnow)  
+    paciente_id = db.Column(db.Integer, db.ForeignKey('Pacientes.id'), nullable=False)
+    fecha_creacion = db.Column(db.Date, default=datetime.utcnow)
 
     paciente = db.relationship('Paciente', backref=db.backref('odontogramas', lazy=True))
