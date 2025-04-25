e93a88b97c5fc59fffd808e8926b31f40f22652c
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-25 00:34:17 -0600

    odontograma 7

--- app/models/Odontograma.py
+++ app/models/Odontograma.py
@@ -1,5 +1,6 @@
 from app import db
 class Odontograma(db.Model):
+     __tablename__ = 'odontograma'
     id = db.Column(db.Integer, primary_key=True)
     paciente_id = db.Column(db.Integer, db.ForeignKey('paciente.id'))
     fecha_creacion = db.Column(db.Date)
