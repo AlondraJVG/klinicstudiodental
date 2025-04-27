c8b3fb913a908421bf594e2100dabc0c9b0966c4
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-27 03:14:12 -0600

    citas 13

--- app/models/CondicionesDentales.py
+++ app/models/CondicionesDentales.py
@@ -6,12 +6,4 @@ class CondicionesDentales(db.Model):
     nombre = db.Column(db.String(100))
     descripcion = db.Column(db.Text)
 
-class Diente(db.Model):
-    __tablename__ = 'dientes'
-    id = db.Column(db.Integer, primary_key=True)
-    numero_diente = db.Column(db.Integer)
-    ubicacion = db.Column(db.String(50))
-    tipo = db.Column(db.String(50))
-
-
 
