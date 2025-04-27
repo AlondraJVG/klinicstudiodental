579c33ee89af19ee92f71a63b1d609ad710abd94
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-27 03:33:06 -0600

    citas 16

--- app/models/condiciones_por_diente.py
+++ app/models/condiciones_por_diente.py
@@ -1,10 +1,15 @@
 # app/models/condiciones_por_diente.py
 from app import db
+from app.models.Diente import Diente
+from app.models.CondicionesDentales import CondicionesDentales
 
 class CondicionesPorDiente(db.Model):
     __tablename__ = 'condiciones_por_diente'
     id = db.Column(db.Integer, primary_key=True)
     diente_id = db.Column(db.Integer, db.ForeignKey('dientes.id'))
-    condicion = db.Column(db.String(255))
+    condicion_id = db.Column(db.Integer, db.ForeignKey('condiciones_dentales.id'))  # Corregido para referencia correcta
+    descripcion = db.Column(db.String(255))  # Asegúrate de que este campo esté definido correctamente
 
-    diente = db.relationship('dientes', backref='condiciones')
+    # Relaciones
+    diente = db.relationship('Diente', backref='condiciones')
+    condicion = db.relationship('CondicionesDentales', backref='condiciones_por_diente')
