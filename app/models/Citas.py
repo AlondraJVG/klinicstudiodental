8deae9f2c666ce19221acb27335b3644ffaee361
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-04-27 02:30:43 -0600

    citas 3

--- app/models/Citas.py
+++ app/models/Citas.py
@@ -2,15 +2,10 @@ from app import db
 
 class Cita(db.Model):
     __tablename__ = 'citas'
-    
     id = db.Column(db.Integer, primary_key=True)
-    paciente_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'), nullable=False)
-    tratamiento_id = db.Column(db.Integer, db.ForeignKey('tratamientos.id'), nullable=True)
-    fecha = db.Column(db.Date, nullable=False)
-    hora = db.Column(db.Time, nullable=False)
-    motivo = db.Column(db.String(255), nullable=False)
-    notas = db.Column(db.Text)
-    estado = db.Column(db.String(50), default="Programada")
-    
-    paciente = db.relationship('Paciente', backref=db.backref('citas', lazy=True))
-    tratamiento = db.relationship('Tratamiento', backref=db.backref('citas', lazy=True))
+    fecha = db.Column(db.DateTime)
+    paciente_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'))
+    tratamiento_id = db.Column(db.Integer, db.ForeignKey('tratamientos.id'))
+
+    paciente = db.relationship('Paciente', backref='citas')
+    tratamiento = db.relationship('Tratamiento', backref='citas')
