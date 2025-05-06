e51472a4b00b6904feb056b6802e15776e9a6ece
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-05-06 00:17:42 -0600

    correo 8

--- app/utils/correo.py
+++ app/utils/correo.py
@@ -1,8 +1,11 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
 from config import Config  # Importa la clase Config
 
 def enviar_correo(destinatario, asunto, cuerpo):
+    
     # Accede a las variables a través de Config
     MAIL_SERVER = Config.MAIL_SERVER
     MAIL_PORT = Config.MAIL_PORT
@@ -11,10 +14,9 @@ def enviar_correo(destinatario, asunto, cuerpo):
     MAIL_PASSWORD = Config.MAIL_PASSWORD
 
     mensaje = MIMEText(cuerpo, "html", "utf-8")
-    mensaje["Subject"] = asunto
-    mensaje["From"] = MAIL_USERNAME
+    mensaje["Subject"] = Header(asunto, "utf-8")
+    mensaje["From"] = formataddr((str(Header("Klinic Studio Dental", "utf-8")), MAIL_USERNAME))
     mensaje["To"] = destinatario
-
     with smtplib.SMTP(MAIL_SERVER, MAIL_PORT) as servidor:
         if MAIL_USE_TLS:
             servidor.starttls()
