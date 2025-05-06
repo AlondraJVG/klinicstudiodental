a1e1b67e2d20b06b732a25706741b73f6ec23410
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-05-06 01:46:28 -0600

    p1

--- app/models/Odontograma.py
+++ app/models/Odontograma.py
@@ -5,7 +5,7 @@ class Odontograma(db.Model):
     __tablename__ = 'odontogramas'  
 
     id = db.Column(db.Integer, primary_key=True)
-    pacientes_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'), nullable=False)
+    pacientes_id = db.Column(db.Integer, db.ForeignKey('paciente.id'), nullable=False)
     fecha_creacion = db.Column(db.Date, default=datetime.utcnow)
 
     paciente = db.relationship('Paciente', backref=db.backref('odontogramas', lazy=True))
