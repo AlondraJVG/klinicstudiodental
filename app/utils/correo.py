4e45deec1bb3cb63c6a1c174245fea9ad27d5f3a
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-05-05 23:54:56 -0600

    correo 4

--- app/utils/correo.py
+++ app/utils/correo.py
@@ -1,8 +1,15 @@
 import smtplib
 from email.mime.text import MIMEText
-from config import MAIL_SERVER, MAIL_PORT, MAIL_USE_TLS, MAIL_USERNAME, MAIL_PASSWORD
+from config import Config  # Importa la clase Config
 
 def enviar_correo(destinatario, asunto, cuerpo):
+    # Accede a las variables a través de Config
+    MAIL_SERVER = Config.MAIL_SERVER
+    MAIL_PORT = Config.MAIL_PORT
+    MAIL_USE_TLS = Config.MAIL_USE_TLS
+    MAIL_USERNAME = Config.MAIL_USERNAME
+    MAIL_PASSWORD = Config.MAIL_PASSWORD
+
     mensaje = MIMEText(cuerpo, "html")
     mensaje["Subject"] = asunto
     mensaje["From"] = MAIL_USERNAME
