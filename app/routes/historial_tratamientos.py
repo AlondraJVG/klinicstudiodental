3ea342aedc8e4c347620fa2d2ee70c70680abcaf
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-05-07 05:38:47 -0600

    prueba historial_clinico 11

--- app/routes/historial_tratamientos.py
+++ app/routes/historial_tratamientos.py
@@ -6,10 +6,10 @@ from app.models.Historial_Tratamientos import HistorialTratamientos
 historial_tratamientos_bp = Blueprint('historial_tratamientos', __name__, url_prefix='/historial')
 
 # Ver historial de tratamientos de un paciente
-@historial_tratamientos_bp.route('/<int:paciente_id>')
+@historial_tratamientos_bp.route('/historial/<int:paciente_id>', methods=['GET'])
 def ver_historial(paciente_id):
     paciente = Paciente.query.get_or_404(paciente_id)
-    historial = HistorialTratamientos.query.filter_by(paciente_id=paciente_id).all()
+    historial = HistorialTratamientos.query.filter_by(paciente_id=paciente_id).all() 
     return render_template('historial/ver_historial.html', paciente=paciente, historial=historial)
 
 # Editar un tratamiento del historial
