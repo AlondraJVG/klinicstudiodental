850c15b16297daf62d0ae2aacdedbde896324354
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-05-14 17:32:40 -0600

    Seguridad 2

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,6 +1,8 @@
 from app import db
 from flask_login import UserMixin
-class Usuario(db.Mode, UserMixinl):
+
+class Usuario(db.Model, UserMixin):
+
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
