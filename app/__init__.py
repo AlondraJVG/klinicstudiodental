b68bee56d7f781ac35054af1d41bead87ce706cc
AlondraJVG <zap0460@zapopan.tecmm.edu.mx>
2025-05-14 18:04:01 -0600

    Seguridad 6

--- app/__init__.py
+++ app/__init__.py
@@ -11,29 +11,27 @@ from app.models.usuario import Usuario
 db = SQLAlchemy()
 mail = Mail()
 login_manager = LoginManager()
+
 login_manager.login_view = 'auth.login'
 
 def create_app():
-    """Crea una instancia de la aplicación Flask."""
     app = Flask(__name__)
-    app.config.from_object(get_config())  # Cargar la configuración según el entorno
+    app.config.from_object(get_config()) 
 
     app.secret_key = 'clave-secreta'
-    app.permanent_session_lifetime = timedelta(minutes=5)  # <-- duración de sesión
-    login_manager.init_app(app)
+    app.permanent_session_lifetime = timedelta(minutes=5)  
 
     db.init_app(app)
     mail.init_app(app)
+    login_manager.init_app(app)
 
 
-    # Filtro personalizado para saltos de línea
     @app.template_filter('nl2br')
     def nl2br_filter(s):
         if not isinstance(s, str):
             return ''
         return Markup(s.replace('\n', '<br>\n'))
 
-    # Importar y registrar blueprints
     from app.routes.auth import auth_bp
     from app.routes.paciente import paciente_bp
     from app.routes.cita import cita_bp
@@ -47,6 +45,5 @@ def create_app():
     app.register_blueprint(historial_tratamientos_bp)
 
     return app
-@login_manager.user_loader
 def load_user(user_id):
     return Usuario.query.get(int(user_id))
